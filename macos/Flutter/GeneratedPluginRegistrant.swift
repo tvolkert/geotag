beda2e731bc3bd292bc9466c9a847bbe0d648f17
Todd Volkert <toddvolkert@Todds-MacBook-Air.local>
2023-11-26 09:02:04 -0800

    Added plugins

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_foundation
+import platform_native
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PlatformNativePlugin.register(with: registry.registrar(forPlugin: "PlatformNativePlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
