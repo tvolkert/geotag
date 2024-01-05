2d4fffa208e5879ca5a14893372d097fdb006f98
Todd Volkert <tvolkert@gmail.com>
2024-01-05 12:06:53 -0800

    Started cleaning up file structure by moving stuff out of main.dart
    * VideoPlayer & associated files
    * DateTimeEditorDialog and associated files

---
+++ lib/src/ui/date_time_editor.dart
@@ -0,0 +1,454 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+/// A widget that allows the user to edit the DateTime associated with a photo
+/// or video.
+///
+/// This widget is meant to be shown via the [show] method.
+class DateTimeEditorDialog extends StatefulWidget {
+  const DateTimeEditorDialog({
+    super.key,
+    required this.initialDateTime,
+  });
+
+  final DateTime? initialDateTime;
+
+  /// Shows a [DateTimeEditorDialog] and returns a future that contains the
+  /// results of the user's edits.
+  ///
+  /// If the user cancels the edits or clicks outside the dialog to dismiss it,
+  /// the returned future will produce a null value.
+  static Future<DateTime?> show(BuildContext context, [DateTime? initialDateTime]) {
+    return showDialog<DateTime>(
+      context: context,
+      builder: (BuildContext context) => DateTimeEditorDialog(initialDateTime: initialDateTime),
+    );
+  }
+
+  @override
+  State<DateTimeEditorDialog> createState() => _DateTimeEditorDialogState();
+}
+
+enum _AmPm {
+  am,
+  pm,
+}
+
+class _DateTimeEditorDialogState extends State<DateTimeEditorDialog> {
+  late int year, defaultYear;
+  late int month, defaultMonth;
+  late int day, defaultDay;
+  late int hour, defaultHour;
+  late int minute, defaultMinute;
+  late _AmPm ampm, defaultAmPm;
+
+  void _handleYearChanged(int value) {
+    setState(() {
+      year = value;
+    });
+  }
+
+  void _handleMonthChanged(int value) {
+    setState(() {
+      month = value;
+    });
+  }
+
+  void _handleDayChanged(int value) {
+    setState(() {
+      day = value;
+    });
+  }
+
+  void _handleHourChanged(int value) {
+    setState(() {
+      hour = value;
+    });
+  }
+
+  void _handleMinuteChanged(int value) {
+    setState(() {
+      minute = value;
+    });
+  }
+
+  void _handleAmPmChanged(_AmPm value) {
+    setState(() {
+      ampm = value;
+    });
+  }
+
+  void _handleCancel() {
+    Navigator.pop<DateTime>(context);
+  }
+
+  void _handleSave() {
+    Navigator.pop<DateTime>(context, currentDateTime);
+  }
+
+  DateTime get currentDateTime {
+    final int hour24 = hour + (ampm == _AmPm.pm ? 12 : 0);
+    return DateTime(year, month, day, hour24, minute);
+  }
+
+  String _formatCurrentDateTime() {
+    final DateFormat format = DateFormat('EEEE, MMMM d, yyyy, h:mm a');
+    return format.format(currentDateTime);
+  }
+
+  _initialize() {
+    final DateTime instant = widget.initialDateTime ?? DateTime.now();
+    year = defaultYear = instant.year;
+    month = defaultMonth = instant.month;
+    day = defaultDay = instant.day;
+    hour = defaultHour = instant.hour % 12;
+    minute = defaultMinute = instant.minute;
+    ampm = defaultAmPm = instant.hour < 12 ? _AmPm.am : _AmPm.pm;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
+  @override
+  void didUpdateWidget(covariant DateTimeEditorDialog oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.initialDateTime != oldWidget.initialDateTime) {
+      _initialize();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      // TODO: Use a Dialog widget instead once the webview handles input correctly.
+      child: Material(
+        // backgroundColor: Colors.white,
+        // surfaceTintColor: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text('Edit date & time', style: TextStyle(fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              Text(_formatCurrentDateTime()),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  _IntEntry(defaultValue: defaultYear, label: 'Year', maxLength: 4, onValueChanged: _handleYearChanged, autofocus: true),
+                  const SizedBox(width: 8),
+                  _IntEntry(defaultValue: defaultMonth, label: 'Month', onValueChanged: _handleMonthChanged),
+                  const SizedBox(width: 8),
+                  _IntEntry(defaultValue: defaultDay, label: 'Day', onValueChanged: _handleDayChanged),
+                  const SizedBox(width: 12),
+                  _IntEntry(defaultValue: defaultHour, label: 'Time', onValueChanged: _handleHourChanged),
+                  const SizedBox(width: 8, child: Center(child: Text(':'))),
+                  _IntEntry(defaultValue: defaultMinute, onValueChanged: _handleMinuteChanged),
+                  const SizedBox(width: 8),
+                  _AmPmEntry(defaultValue: defaultAmPm, onValueChanged: _handleAmPmChanged),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  const SizedBox(width: 500),
+                  _ActionButton(label: 'Cancel', onPressed: _handleCancel),
+                  const SizedBox(width: 10),
+                  _ActionButton(label: 'Save', onPressed: _handleSave),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ValidatingTextEditingController<T> extends TextEditingController {
+  _ValidatingTextEditingController(this.onValueChanged);
+
+  final ValueChanged<T> onValueChanged;
+  bool _doingSetup = false;
+
+  @protected
+  bool get doingSetup => _doingSetup;
+
+  void invokeSetupCallback(VoidCallback callback) {
+    _doingSetup = true;
+    try {
+      callback();
+    } finally {
+      _doingSetup = false;
+    }
+  }
+}
+
+class _IntValidatingTextEditingController extends _ValidatingTextEditingController<int> {
+  _IntValidatingTextEditingController(super.onValueChanged);
+
+  @override
+  set value(TextEditingValue newValue) {
+    final String newText = newValue.text;
+    final bool textChanged = newText != value.text;
+    final int? intValue = int.tryParse(newValue.text);
+    if (newText.isEmpty || (intValue != null && intValue >= 0)) {
+      super.value = newValue;
+      if (!doingSetup && textChanged && intValue != null) {
+        onValueChanged(intValue);
+      }
+    }
+  }
+}
+
+class _IntEntry extends StatefulWidget {
+  const _IntEntry({
+    required this.defaultValue,
+    this.label = '',
+    this.maxLength = 2,
+    required this.onValueChanged,
+    this.autofocus = false,
+  });
+
+  final int defaultValue;
+  final String label;
+  final int maxLength;
+  final ValueChanged<int> onValueChanged;
+  final bool autofocus;
+
+  @override
+  State<_IntEntry> createState() => _IntEntryState();
+}
+
+class _IntEntryState extends State<_IntEntry> {
+  late final FocusNode _focusNode;
+  late final _IntValidatingTextEditingController _controller;
+  bool _hasFocus = false;
+
+  void _handleFocusChanged() {
+    if (_focusNode.hasFocus != _hasFocus) {
+      setState(() {
+        _hasFocus = _focusNode.hasFocus;
+      });
+      if (_hasFocus) {
+        _controller.selection = TextSelection(baseOffset: 0, extentOffset: _controller.text.length);
+      } else if (_controller.text.isEmpty) {
+        _controller.text = '${widget.defaultValue}';
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_handleFocusChanged);
+    _controller = _IntValidatingTextEditingController(widget.onValueChanged);
+    _controller.invokeSetupCallback(() {
+      _controller.text = '${widget.defaultValue}';
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant _IntEntry oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    assert(widget.onValueChanged == oldWidget.onValueChanged);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.removeListener(_handleFocusChanged);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _RawEntry(
+      autofocus: widget.autofocus,
+      focusNode: _focusNode,
+      controller: _controller,
+      onTap: null,
+      maxLength: widget.maxLength,
+      labelText: widget.label,
+      hasFocus: _hasFocus,
+    );
+  }
+}
+
+class _AmPmValidatingTextEditingController extends _ValidatingTextEditingController<_AmPm> {
+  _AmPmValidatingTextEditingController(super.onValueChanged);
+
+  static const TextSelection selectAll = TextSelection(baseOffset: 0, extentOffset: 2);
+
+  @override
+  set value(TextEditingValue newValue) {
+    switch (newValue.text.toLowerCase()) {
+      case 'a':
+      case 'am':
+        super.value = const TextEditingValue(text: 'AM', selection: selectAll);
+        if (!doingSetup) {
+          onValueChanged(_AmPm.am);
+        }
+      case 'p':
+      case 'pm':
+        super.value = const TextEditingValue(text: 'PM', selection: selectAll);
+        if (!doingSetup) {
+          onValueChanged(_AmPm.pm);
+        }
+    }
+  }
+}
+
+class _AmPmEntry extends StatefulWidget {
+  const _AmPmEntry({
+    required this.defaultValue,
+    required this.onValueChanged,
+  });
+
+  final _AmPm defaultValue;
+  final ValueChanged<_AmPm> onValueChanged;
+
+  @override
+  State<_AmPmEntry> createState() => _AmPmEntryState();
+}
+
+class _AmPmEntryState extends State<_AmPmEntry> {
+  late final FocusNode _focusNode;
+  late final _AmPmValidatingTextEditingController _controller;
+  bool _hasFocus = false;
+
+  void _handleFocusChanged() {
+    if (_focusNode.hasFocus != _hasFocus) {
+      setState(() {
+        _hasFocus = _focusNode.hasFocus;
+      });
+      if (_hasFocus) {
+        _controller.selection = TextSelection(baseOffset: 0, extentOffset: _controller.text.length);
+      }
+    }
+  }
+
+  void _handleTap() {
+    switch (_controller.text) {
+      case 'AM':
+        _controller.text = 'PM';
+      case 'PM':
+        _controller.text = 'AM';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_handleFocusChanged);
+    _controller = _AmPmValidatingTextEditingController(widget.onValueChanged);
+    _controller.invokeSetupCallback(() {
+      _controller.text = widget.defaultValue == _AmPm.am ? 'AM' : 'PM';
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.removeListener(_handleFocusChanged);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _RawEntry(
+      autofocus: false,
+      focusNode: _focusNode,
+      controller: _controller,
+      onTap: _handleTap,
+      maxLength: 2,
+      labelText: '',
+      hasFocus: _hasFocus,
+    );
+  }
+}
+
+class _RawEntry extends StatelessWidget {
+  const _RawEntry({
+    required this.autofocus,
+    required this.focusNode,
+    required this.controller,
+    required this.onTap,
+    required this.maxLength,
+    required this.labelText,
+    required this.hasFocus,
+  });
+
+  final bool autofocus;
+  final FocusNode focusNode;
+  final TextEditingController controller;
+  final GestureTapCallback? onTap;
+  final int maxLength;
+  final String labelText;
+  final bool hasFocus;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 96,
+      child: TextField(
+        autofocus: autofocus,
+        focusNode: focusNode,
+        controller: controller,
+        maxLength: maxLength,
+        onTap: onTap,
+        maxLines: 1,
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: const TextStyle(fontSize: 15),
+          counterText: '',
+          filled: true,
+          fillColor: hasFocus ? const Color(0xffe7e9ea) : const Color(0xfff1f3f4),
+        ),
+      ),
+    );
+  }
+}
+
+class _ActionButton extends StatelessWidget {
+  const _ActionButton({
+    required this.label,
+    this.onPressed,
+  });
+
+  final String label;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(4),
+      child: ColoredBox(
+        color: const Color.fromARGB(255, 56, 113, 224),
+        child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            surfaceTintColor: Colors.transparent,
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.all(16),
+            textStyle: const TextStyle(fontSize: 14),
+          ),
+          onPressed: onPressed,
+          child: Text(label),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/ui/video_player.dart
@@ -0,0 +1,466 @@
+import 'dart:async';
+import 'dart:ui' as ui;
+
+import 'package:file/file.dart';
+import 'package:file/local.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart' as vp;
+
+import '../extensions/duration.dart';
+import '../model/db.dart';
+
+class VideoPlaySymbol extends StatelessWidget {
+  const VideoPlaySymbol({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final double size = (constraints.biggest / 5).shortestSide;
+          return SizedBox(
+            width: size,
+            height: size,
+            child: const DecoratedBox(
+              decoration: BoxDecoration(
+                color: Color(0xaa000000),
+                borderRadius: BorderRadius.all(Radius.circular(10))
+              ),
+              child: CustomPaint(painter: _PlayPauseButtonPainter(value: _PlayPauseButtonPainter.playSymbol)),
+            ),
+          );
+        }
+      ),
+    );
+  }
+}
+
+final class VideoPlayerPlayPauseController {
+  VoidCallback? _onPlayPause;
+
+  void playPause() {
+    _onPlayPause?.call();
+  }
+}
+
+class VideoPlayer extends StatefulWidget {
+  VideoPlayer({
+    super.key,
+    required this.row,
+    required this.playPauseController,
+    this.fs = const LocalFileSystem(),
+  }) : assert(row.type == MediaType.video);
+
+  final DbRow row;
+  final VideoPlayerPlayPauseController playPauseController;
+  final FileSystem fs;
+
+  @override
+  State<VideoPlayer> createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  late vp.VideoPlayerController _controller;
+  bool _isControllerInitialized = false;
+
+  void _handlePlayPause() {
+    setState(() {
+      if (_controller.value.isPlaying) {
+        _controller.pause();
+      } else {
+        _controller.play();
+      }
+    });
+  }
+
+  void _initializeVideoController() {
+    _isControllerInitialized = false;
+    _controller = vp.VideoPlayerController.file(widget.fs.file(widget.row.path));
+    _controller.setLooping(true);
+    _controller.initialize().then((void _) {
+      setState(() {
+        _isControllerInitialized = true;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeVideoController();
+    widget.playPauseController._onPlayPause = _handlePlayPause;
+  }
+
+  @override
+  void didUpdateWidget(covariant VideoPlayer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.row.path != oldWidget.row.path) {
+      // TODO: Can we just update it instead of disposing and re-creating?
+      _controller.pause();
+      _controller.dispose();
+      _initializeVideoController();
+    }
+    if (widget.playPauseController != oldWidget.playPauseController) {
+      oldWidget.playPauseController._onPlayPause = null;
+      widget.playPauseController._onPlayPause = _handlePlayPause;
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.playPauseController._onPlayPause = null;
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget result = Image.file(widget.fs.file(widget.row.photoPath));
+    if (_isControllerInitialized) {
+      result = Center(
+        child: AspectRatio(
+          aspectRatio: _controller.value.aspectRatio,
+          child: GestureDetector(
+            onTap: _handlePlayPause,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                vp.VideoPlayer(_controller),
+                _VideoProgressMonitor(
+                  controller: _controller,
+                  forceIsVisible: !_controller.value.isPlaying,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+    return ColoredBox(
+      color: Colors.black,
+      child: result,
+    );
+  }
+}
+
+class _VideoProgressMonitor extends StatefulWidget {
+  const _VideoProgressMonitor({
+    required this.controller,
+    required this.forceIsVisible,
+  });
+
+  final vp.VideoPlayerController controller;
+  final bool forceIsVisible;
+
+  @override
+  State<_VideoProgressMonitor> createState() => _VideoProgressMonitorState();
+}
+
+class _VideoProgressMonitorState extends State<_VideoProgressMonitor> {
+  double _progress = 0;
+  String _positionText = '';
+  String _durationText = '';
+  bool _isPlaying = false;
+  bool _pointerDown = false;
+  bool _isVisible = false;
+  Timer? _hoverTimer;
+
+  static const Duration _fadeDuration = Duration(milliseconds: 100);
+  static const Duration _visibleDuration = Duration(seconds: 3);
+
+  static const BoxDecoration _gradientDecoration = BoxDecoration(
+    gradient: LinearGradient(
+      colors: <Color>[Color(0x99000000), Colors.transparent],
+      begin: Alignment.bottomCenter,
+      end: Alignment.center,
+    ),
+  );
+
+  void _handleProgressUpdate() {
+    setState(() {
+      final vp.VideoPlayerValue value = widget.controller.value;
+      _progress = value.position.inMilliseconds / value.duration.inMilliseconds;
+      _positionText = value.position.toSecondsPrecision();
+      _durationText = value.duration.toSecondsPrecision();
+      _isPlaying = value.isPlaying;
+    });
+  }
+
+  void _handlePlayPause() {
+    if (_isPlaying) {
+      widget.controller.pause();
+    } else {
+      widget.controller.play();
+    }
+  }
+
+  void _consumeTap() {
+    // Consume taps over the seeker so it doesn't play/pause the video
+  }
+
+  void _handlePointerDown(PointerDownEvent event) {
+    _seekToPosition(event);
+    setState(() {
+      _pointerDown = true;
+    });
+  }
+
+  void _handlePointerUp(PointerUpEvent event) {
+    setState(() {
+      _pointerDown = false;
+    });
+  }
+
+  void _handlePointerMove(PointerMoveEvent event) {
+    assert(_pointerDown);
+    _seekToPosition(event);
+  }
+
+  void _handlePointerHover(PointerHoverEvent event) {
+    setState(() {
+      _isVisible = true;
+    });
+    _resetHoverTimer();
+  }
+
+  void _seekToPosition(PointerEvent event) {
+    final Duration total = widget.controller.value.duration;
+    final double percentage = event.localPosition.dx / context.size!.width;
+    final Duration seekTo = total * percentage;
+    widget.controller.seekTo(seekTo);
+  }
+
+  void _resetHoverTimer() {
+    _hoverTimer?.cancel();
+    _hoverTimer = Timer(_visibleDuration, () {
+      assert(mounted);
+      setState(() {
+        _isVisible = false;
+        _hoverTimer = null;
+      });
+    });
+  }
+
+  bool get isVisible => widget.forceIsVisible || _isVisible;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(_handleProgressUpdate);
+    _handleProgressUpdate();
+  }
+
+  @override
+  void didUpdateWidget(covariant _VideoProgressMonitor oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.controller != oldWidget.controller) {
+      oldWidget.controller.removeListener(_handleProgressUpdate);
+      widget.controller.addListener(_handleProgressUpdate);
+      _handleProgressUpdate();
+    }
+    if (widget.forceIsVisible && !oldWidget.forceIsVisible) {
+      _isVisible = true;
+      _resetHoverTimer();
+    }
+  }
+
+  @override
+  void dispose() {
+    _hoverTimer?.cancel();
+    widget.controller.removeListener(_handleProgressUpdate);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerHover: _handlePointerHover,
+      child: AnimatedOpacity(
+        opacity: isVisible ? 1 : 0,
+        duration: _fadeDuration,
+        child: DecoratedBox(
+          decoration: _gradientDecoration,
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 21,
+                  child: GestureDetector(
+                    onTap: _consumeTap,
+                    child: MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: Listener(
+                        behavior: HitTestBehavior.opaque,
+                        onPointerDown: _handlePointerDown,
+                        onPointerUp: _handlePointerUp,
+                        onPointerMove: _pointerDown ? _handlePointerMove : null,
+                        child: AbsorbPointer(
+                          child: Align(
+                            alignment: const Alignment(-1, 0),
+                            child: SizedBox(
+                              height: 5,
+                              child: LinearProgressIndicator(
+                                value: _progress,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 3),
+                  child: DefaultTextStyle.merge(
+                    style: const TextStyle(color: Colors.white),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        _PlayPauseButton(isPlaying: _isPlaying, onPlayPause: _handlePlayPause),
+                        const SizedBox(width: 10),
+                        ConstrainedBox(constraints: const BoxConstraints(minWidth: 45), child: Text(_positionText)),
+                        ConstrainedBox(constraints: const BoxConstraints(minWidth: 10), child: const Text('/')),
+                        ConstrainedBox(constraints: const BoxConstraints(minWidth: 45), child: Text(_durationText)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayPauseButton extends ImplicitlyAnimatedWidget {
+  const _PlayPauseButton({
+    required this.isPlaying,
+    required this.onPlayPause,
+  }) : super(duration: const Duration(milliseconds: 200));
+
+  final bool isPlaying;
+  final VoidCallback onPlayPause;
+
+  @override
+  AnimatedWidgetBaseState<_PlayPauseButton> createState() => _PlayPauseButtonState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<bool>('isPlaying', isPlaying));
+  }
+}
+
+class _PlayPauseButtonState extends AnimatedWidgetBaseState<_PlayPauseButton> {
+  Tween<double>? _value;
+
+  @override
+  void forEachTween(TweenVisitor<dynamic> visitor) {
+    _value = visitor(
+      _value,
+      widget.isPlaying ? _PlayPauseButtonPainter.pauseSymbol : _PlayPauseButtonPainter.playSymbol,
+      (dynamic value) => Tween<double>(begin: value as double)
+    ) as Tween<double>?;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: widget.onPlayPause,
+        child: SizedBox(
+          width: 40,
+          height: 50,
+          child: CustomPaint(
+            painter: _PlayPauseButtonPainter(value: _value!.evaluate(animation)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder description) {
+    super.debugFillProperties(description);
+    description.add(DiagnosticsProperty<Tween<double>>('value', _value, defaultValue: null));
+  }
+}
+
+class _PlayPauseButtonPainter extends CustomPainter {
+  const _PlayPauseButtonPainter({
+    required this.value,
+  });
+
+  final double value;
+
+  static const double playSymbol = 0;
+  static const double pauseSymbol = 1;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    ui.Paint paint = ui.Paint()
+        ..style = ui.PaintingStyle.fill
+        ..color = Colors.white
+        ;
+    List<Offset> pauseOffsets = <Offset>[
+      Offset(size.width / 3, size.height / 3),
+      Offset(size.width * 4 / 9, size.height / 3),
+      Offset(size.width * 4 / 9, size.height * 2 / 3),
+      Offset(size.width / 3, size.height * 2 / 3),
+      Offset(size.width * 5 / 9, size.height / 3),
+      Offset(size.width * 2 / 3, size.height / 3),
+      Offset(size.width * 2 / 3, size.height * 2 / 3),
+      Offset(size.width * 5 / 9, size.height * 2 / 3),
+    ];
+    List<Offset> playOffsets = <Offset>[
+      Offset(size.width / 3, size.height / 3),
+      Offset(size.width / 2, size.height * 5 / 12),
+      Offset(size.width / 2, size.height * 7 / 12),
+      Offset(size.width / 3, size.height * 2 / 3),
+      Offset(size.width / 2, size.height * 5 / 12),
+      Offset(size.width * 2 / 3, size.height / 2),
+      Offset(size.width * 2 / 3, size.height / 2),
+      Offset(size.width / 2, size.height * 7 / 12),
+    ];
+
+    List<Offset> offsets;
+    if (value == playSymbol) {
+      offsets = playOffsets;
+    } else if (value == pauseSymbol) {
+      offsets = pauseOffsets;
+    } else {
+      offsets = List<Offset>.generate(playOffsets.length, (int index) {
+        return Offset.lerp(playOffsets[index], pauseOffsets[index], value)!;
+      });
+    }
+
+    ui.Path path = ui.Path()
+        ..moveTo(offsets[0].dx, offsets[0].dy)
+        ..lineTo(offsets[1].dx, offsets[1].dy)
+        ..lineTo(offsets[2].dx, offsets[2].dy)
+        ..lineTo(offsets[3].dx, offsets[3].dy)
+        ..close()
+        ..moveTo(offsets[4].dx, offsets[4].dy)
+        ..lineTo(offsets[5].dx, offsets[5].dy)
+        ..lineTo(offsets[6].dx, offsets[6].dy)
+        ..lineTo(offsets[7].dx, offsets[7].dy)
+        ..close()
+        ;
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant _PlayPauseButtonPainter oldDelegate) {
+    return value != oldDelegate.value;
+  }
+}
