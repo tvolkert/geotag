13ce4d67b838d8b4c224706e9a9dd7ade54ee4cb
Todd Volkert <tvolkert@gmail.com>
2024-01-13 21:40:11 -0800

    Implemented basic PhotoPile widget

--- lib/src/ui/photo_pile.dart
+++ lib/src/ui/photo_pile.dart
@@ -1,5 +1,8 @@
+import 'dart:math' as math;
+
 import 'package:flutter/widgets.dart';
 
+import '../model/files.dart';
 import '../model/media.dart';
 
 class PhotoPile extends StatelessWidget {
@@ -10,8 +13,27 @@ class PhotoPile extends StatelessWidget {
 
   final Iterable<MediaItem> items;
 
+  static const List<double> _rotations = <double>[0.05, -0.1];
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    int i = 0;
+    return Padding(
+      padding: const EdgeInsets.all(50),
+      child: Center(
+        child: Stack(
+          children: <Widget>[
+            ...items.take(items.length - 1).map<Widget>((MediaItem item) {
+              final double rotation = _rotations[i++];
+              return Transform.rotate(
+                angle: math.pi * rotation,
+                child: Image.file(FilesBinding.instance.fs.file(item.photoPath)),
+              );
+            }),
+            Image.file(FilesBinding.instance.fs.file(items.last.photoPath)),
+          ],
+        ),
+      ),
+    );
   }
 }
