8ee42575549589728435559e46732560494f097a
Todd Volkert <tvolkert@gmail.com>
2024-02-01 21:40:44 -0800

    Uncommented tests that were accidentally commented out

--- test/ui/thumbnail_list_test.dart
+++ test/ui/thumbnail_list_test.dart
@@ -9,65 +9,65 @@ import 'package:geotag/src/ui/thumbnail_list.dart';
 import '../src/common.dart';
 
 Future<void> main() async {
-  // testGeotag('ThumbnailList produces no thumbnails if items is empty', (WidgetTester tester) async {
-  //   await tester.pumpWidget(
-  //     const MaterialApp(
-  //       home: ThumbnailList(),
-  //     ),
-  //   );
-  //   expect(find.byType(Thumbnail), findsNothing);
-  // });
+  testGeotag('ThumbnailList produces no thumbnails if items is empty', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: ThumbnailList(),
+      ),
+    );
+    expect(find.byType(Thumbnail), findsNothing);
+  });
 
-  // testGeotag('ThumbnailList produces thumbnails if items is not empty', (WidgetTester tester) async {
-  //   final ImageReferences images = tester.loadImages();
-  //   await MediaBinding.instance.items.addFiles(images.paths).drain<void>();
-  //   await tester.pumpWidget(
-  //     const MaterialApp(
-  //       home: ThumbnailList(),
-  //     ),
-  //   );
-  //   expect(find.byType(Thumbnail), findsNWidgets(images.length));
-  // });
+  testGeotag('ThumbnailList produces thumbnails if items is not empty', (WidgetTester tester) async {
+    final ImageReferences images = tester.loadImages();
+    await MediaBinding.instance.items.addFiles(images.paths).drain<void>();
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: ThumbnailList(),
+      ),
+    );
+    expect(find.byType(Thumbnail), findsNWidgets(images.length));
+  });
 
-  // testGeotag('ThumbnailList produces thumbnails when items are added to root', (WidgetTester tester) async {
-  //   final ImageReferences images = tester.loadImages();
-  //   await tester.pumpWidget(
-  //     const MaterialApp(
-  //       home: ThumbnailList(),
-  //     ),
-  //   );
-  //   expect(find.byType(Thumbnail), findsNothing);
+  testGeotag('ThumbnailList produces thumbnails when items are added to root', (WidgetTester tester) async {
+    final ImageReferences images = tester.loadImages();
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: ThumbnailList(),
+      ),
+    );
+    expect(find.byType(Thumbnail), findsNothing);
 
-  //   await MediaBinding.instance.items.addFiles(images.paths).drain<void>();
-  //   await tester.pump();
-  //   expect(find.byType(Thumbnail), findsNWidgets(images.length));
-  // });
+    await MediaBinding.instance.items.addFiles(images.paths).drain<void>();
+    await tester.pump();
+    expect(find.byType(Thumbnail), findsNWidgets(images.length));
+  });
 
-  // testGeotag('filter by missing date works', (WidgetTester tester) async {
-  //   final ImageReferences images = tester.loadImages();
-  //   await MediaBinding.instance.items.addFiles(images.paths).drain<void>();
-  //   await tester.pumpWidget(
-  //     const MaterialApp(
-  //       home: ThumbnailList(),
-  //     ),
-  //   );
-  //   await tester.tap(find.byTooltip('Show only missing date'));
-  //   await tester.pump();
-  //   expect(find.byType(Thumbnail), findsNWidgets(3));
-  // });
+  testGeotag('filter by missing date works', (WidgetTester tester) async {
+    final ImageReferences images = tester.loadImages();
+    await MediaBinding.instance.items.addFiles(images.paths).drain<void>();
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: ThumbnailList(),
+      ),
+    );
+    await tester.tap(find.byTooltip('Show only missing date'));
+    await tester.pump();
+    expect(find.byType(Thumbnail), findsNWidgets(3));
+  });
 
-  // testGeotag('filter by missing geo works', (WidgetTester tester) async {
-  //   final ImageReferences images = tester.loadImages();
-  //   await MediaBinding.instance.items.addFiles(images.paths).drain<void>();
-  //   await tester.pumpWidget(
-  //     const MaterialApp(
-  //       home: ThumbnailList(),
-  //     ),
-  //   );
-  //   await tester.tap(find.byTooltip('Show only missing geotag'));
-  //   await tester.pump();
-  //   expect(find.byType(Thumbnail), findsNWidgets(3));
-  // });
+  testGeotag('filter by missing geo works', (WidgetTester tester) async {
+    final ImageReferences images = tester.loadImages();
+    await MediaBinding.instance.items.addFiles(images.paths).drain<void>();
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: ThumbnailList(),
+      ),
+    );
+    await tester.tap(find.byTooltip('Show only missing geotag'));
+    await tester.pump();
+    expect(find.byType(Thumbnail), findsNWidgets(3));
+  });
 
   testGeotag('Adding event to multiple items while the no-event filter is applied', (WidgetTester tester) async {
     final ImageReferences images = tester.loadImages();
